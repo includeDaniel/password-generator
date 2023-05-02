235814bbdc34e2e46444729c5a6d6db960c3c682
Daniel Silva <danielzcomputacao@gmail.com>
2023-05-01 22:02:57 -0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // the parameter 'number' is the amount of digits you want your password to be
 
-function passwordGenerator(number) {
+module.exports = function passwordGenerator(number) {
     var password = "";
     for (var i = 1; i <= number; i++) {
         var n = Math.floor(Math.random() * 9)
