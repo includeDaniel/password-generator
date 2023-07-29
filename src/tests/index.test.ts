2bac30f0ec06f67d169b53b6498fded4ab1d6bfe
Daniel Silva <danielzcomputacao@gmail.com>
2023-07-29 16:40:13 -0300

    publish.yml

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -9,8 +9,4 @@ describe("passwordGenerator", () => {
         const n = passwordGenerator(5);
         expect(true).toBe(n > 9999 && n < 100000);
     });
-    test("should return a number with 25 digits", () => {
-        const n = passwordGenerator(10);
-        expect(true).toBe(n > 999999999 && n < 10000000000);
-    });
 });
